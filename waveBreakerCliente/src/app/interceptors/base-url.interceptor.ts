069cd6a325280177eb322507b4f1cb2fa01df32b
adgarola20 <adrygarcia144@gmail.com>
2023-06-12 21:01:22 +0200

    url cambio

--- waveBreakerCliente/src/app/interceptors/base-url.interceptor.ts
+++ waveBreakerCliente/src/app/interceptors/base-url.interceptor.ts
@@ -2,7 +2,7 @@ import { HttpInterceptorFn } from '@angular/common/http';
 
 export const baseUrlInterceptor: HttpInterceptorFn = (req, next) => {
   const reqClone = req.clone({
-    url: `http://localhost:3000/${req.url}`,
+    url: `http://vps-7911aa9f.vps.ovh.net:3000/${req.url}`,
   });
   return next(reqClone);
 };
